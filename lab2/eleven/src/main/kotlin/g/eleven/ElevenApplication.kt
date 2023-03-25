11a451a01e0c4b3ad240463bf3cf905d061e9af5
sarmad <raeessarmad200@gmail.com>
2023-03-25 14:49:33 +0500

    feat: add server module

---
+++ lab2/eleven/src/main/kotlin/g/eleven/ElevenApplication.kt
@@ -0,0 +1,11 @@
+package g.eleven
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ElevenApplication
+
+fun main(args: Array<String>) {
+  runApplication<ElevenApplication>(*args)
+}
